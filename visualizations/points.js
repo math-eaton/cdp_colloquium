58a6b33482244ce543811f76bf4570860f005546
math-eaton <mheaton@ciesin.columbia.edu>
2023-07-30 23:43:18 -0400

    filter nys and voronoi poly

--- visualizations/points.js
+++ visualizations/points.js
@@ -2,11 +2,6 @@ const mapWidth = 1280;
 const mapHeight = 720;
 const mapPadding = 5;
 
-// Define the projection for the map
-const projection = d3.geoAlbersUsa()
-  .translate([mapWidth / 2, mapHeight / 2])
-  .scale(1500);
-
 // Create the SVG container for the map
 const svg = d3.select('#map-container')
   .append('svg')
@@ -14,15 +9,27 @@ const svg = d3.select('#map-container')
   .attr('height', mapHeight);
 
 // Load the US states GeoJSON data
-d3.json('data/states_110m.json').then(function (states) {
-  // Create a path generator for the map projection
+d3.json('data/states_lakes_50m.json').then(function (states) {
+  // Filter the states to include only the record for New York State
+  const nyState = states.features.find(function (state) {
+    return state.properties.name === 'New York';
+  });
+
+  // Create a GeoJSON object with only the geometry of New York State
+  const nyGeoJSON = {
+    type: 'FeatureCollection',
+    features: [nyState],
+  };
+
+  // Create a path generator for the map projection centered on New York State
+  const projection = d3.geoAlbers()
+    .fitExtent([[mapPadding, mapPadding], [mapWidth - mapPadding, mapHeight - mapPadding]], nyGeoJSON);
+
   const path = d3.geoPath().projection(projection);
 
-  // Draw the state boundaries as a basemap
-  svg.selectAll('.state')
-    .data(states.features)
-    .enter()
-    .append('path')
+  // Draw the state boundary for New York
+  svg.append('path')
+    .datum(nyState)
     .attr('class', 'state')
     .attr('d', path);
 
@@ -33,44 +40,49 @@ d3.json('data/states_110m.json').then(function (states) {
       d.centroid_lng = +d.centroid_lng;
       d.centroid_lat = +d.centroid_lat;
     });
-    console.log("Loaded CSV");
 
-    // Draw points on the map based on the data
-    const batchPoints = svg.selectAll('.point')
-      .data(data)
+    // Filter the CSV data to include only records with ", NY" in the "name" column
+    const filteredData = data.filter(function (d) {
+      return d.name.includes(', NY');
+    });
+
+    // Create Voronoi polygons based on the points using d3-delaunay
+    const points = filteredData.map(d => projection([d.centroid_lng, d.centroid_lat]));
+    const delaunay = d3.Delaunay.from(points);
+    const voronoi = delaunay.voronoi([0, 0, mapWidth, mapHeight]);
+
+    // Draw Voronoi polygons on the map
+    svg.selectAll('.voronoi')
+      .data(filteredData)
       .enter()
-      .append('circle')
-      .filter(function (d) {
-        const projected = projection([d.centroid_lng, d.centroid_lat]);
-        return projected !== null;
-      })
-      .attr('class', 'point')
-      .attr('cx', function (d) { return projection([d.centroid_lng, d.centroid_lat])[0]; })
-      .attr('cy', function (d) { return projection([d.centroid_lng, d.centroid_lat])[1]; })
-      .attr('r', 0.25)
-      .style('fill', 'rgba(152, 251, 152, 0.865)');
+      .append('path')
+      .attr('class', 'voronoi')
+      .attr('d', (d, i) => voronoi.renderCell(i))
+      .style('fill', 'none')
+      .style('stroke', 'blue')
+      .style('stroke-width', 1);
 
-    // Apply a debounce function to delay rendering points
-    const debounceRender = debounce(function () {
-      batchPoints.attr('visibility', 'visible');
-    }, 200); // Adjust the delay time as needed
+    // Draw points on the map based on the filtered data
+    const batchPoints = svg.selectAll('.point')
+    .data(filteredData)
+    .enter()
+    .append('circle')
+    .filter(function (d) {
+      const projected = projection([d.centroid_lng, d.centroid_lat]);
+      return projected !== null;
+    })
+    .attr('class', 'point')
+    .attr('cx', function (d) { return projection([d.centroid_lng, d.centroid_lat])[0]; })
+    .attr('cy', function (d) { return projection([d.centroid_lng, d.centroid_lat])[1]; })
+    .attr('r', 1)
+    .style('fill', 'rgba(152, 251, 152, 0.865)')
+    .style('visibility', 'hidden'); // Hide the points initially
 
-    // Call the debounced rendering function
-    debounceRender();
-  });
+  // Use requestAnimationFrame for smoother rendering
+  function renderPoints() {
+    batchPoints.style('visibility', 'visible');
+  }
+  requestAnimationFrame(renderPoints);
+
+    });
 });
-
-// Debounce function to delay rendering
-function debounce(func, wait) {
-  let timeout;
-  return function () {
-    const context = this;
-    const args = arguments;
-    const later = function () {
-      timeout = null;
-      func.apply(context, args);
-    };
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-  };
-}
