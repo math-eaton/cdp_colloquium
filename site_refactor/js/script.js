0416408ef5a4ac447254e6a8a5439db70e2c328b
math-eaton <mheaton@ciesin.columbia.edu>
2023-08-04 02:46:28 -0400

    floater svg updates in real time

--- site_refactor/js/script.js
+++ site_refactor/js/script.js
@@ -27,41 +27,78 @@ window.onload = function() {
     for (var i = 0; i < floaters.length; i++) {
         floaters[i].style.top = Math.random() * 80 + '%'; // limit to 80%
         floaters[i].style.left = Math.random() * 80 + '%'; // limit to 80%
+    }
 
+    // make floaters draggable
+    $( function() {
+        $( ".floating" ).draggable();
+    } );
+
+    // randomly delay the start of floater wobbling animation
+    var floaters = document.getElementsByClassName('floating');
+    for (var i = 0; i < floaters.length; i++) {
         // Random wobbling
-        var duration = Math.random() * (8 - 3) + 3; // Random duration between 8 and 3 seconds
+        var duration = Math.random() * 2 + 1; // Random duration between 1 and 3 seconds
         var delay = Math.random() * 1; // Random delay up to 1 second
         floaters[i].style.animationDuration = duration + 's';
         floaters[i].style.animationDelay = delay + 's';
     }
-    
-    // After positioning the floating divs, draw the lines
-    var floaters = document.getElementsByClassName('floating');
-    var lineCanvas = document.getElementById('lineCanvas');
-    var d = ''; // This will hold the data for the 'd' attribute of the path
-    for (var i = 0; i < floaters.length; i++) {
-        var rect = floaters[i].getBoundingClientRect();
-        var centerX = rect.left + rect.width / 2;
-        var centerY = rect.top + rect.height / 2;
-        if (i === 0) {
-            // Move to the first point
-            d += 'M ' + centerX + ' ' + centerY;
-        } else {
-            // Draw a cubic Bezier curve to the next point
-            d += ' C ' + centerX + ' ' + centerY + ', ' + centerX + ' ' + centerY + ', ' + centerX + ' ' + centerY;
-        }
-    }
-    // Create the path element and set its attributes
-    var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    path.setAttribute('d', d);
-    // path.setAttribute('stroke', '#eeff00');
-    // path.setAttribute('stroke-width', '2');
-    path.setAttribute('fill', '#eeff00');
-    path.setAttribute('fill-opacity', '0.3');
 
-    // Add the path to the SVG
-    lineCanvas.appendChild(path);
+    // Create the SVG element
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.style.width = '100%';
+    svg.style.height = '100%';
+    svg.style.position = 'absolute';
+    svg.style.top = '0';
+    svg.style.left = '0';
+    svg.style.zIndex = '1000';
+    svg.style.pointerEvents = 'none'; 
+    document.body.appendChild(svg);
+
+    // Create the line element
+    var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+    // line.setAttribute('fill', '#eeff00');
+    // line.setAttribute('fill-opacity', '0.3');
+    line.setAttribute('stroke', '#eeff00');
+    line.setAttribute('stroke-width', '2');
+    svg.appendChild(line);
+
+ // Start the animation loop
+requestAnimationFrame(function updateLines() {
+    var floaters = document.getElementsByClassName('floating');
+    var lines = svg.getElementsByTagName('line');
+    
+    // If there are not enough lines, create more
+    while (lines.length < floaters.length - 1) {
+        var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+        line.setAttribute('stroke', '#eeff00');
+        line.setAttribute('stroke-width', '2');
+        svg.appendChild(line);
+    }
+    
+    // Update the position of each line
+    for (var i = 0; i < floaters.length - 1; i++) {
+        var rect1 = floaters[i].getBoundingClientRect();
+        var x1 = rect1.left + window.scrollX + rect1.width / 2;
+        var y1 = rect1.top + window.scrollY + rect1.height / 2;
+        
+        var rect2 = floaters[i + 1].getBoundingClientRect();
+        var x2 = rect2.left + window.scrollX + rect2.width / 2;
+        var y2 = rect2.top + window.scrollY + rect2.height / 2;
+        
+        lines[i].setAttribute('x1', x1);
+        lines[i].setAttribute('y1', y1);
+        lines[i].setAttribute('x2', x2);
+        lines[i].setAttribute('y2', y2);
+    }
+    
+    // Call this function again on the next frame
+    requestAnimationFrame(updateLines);
+});
+
+
 };
+
 
 // make floaters draggable
 $( function() {
